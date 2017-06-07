a0fa89edb8d2764f09357342124dfc19359be877
Filipe Calasans <filipe.calasans@gmail.com>
2017-06-07 00:15:59 -0300

    use all googlefinance available parameters

--- spread_sheet.py
+++ spread_sheet.py
@@ -16,6 +16,7 @@ try:
 except ImportError:
     flags = None
 
+
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
@@ -53,86 +54,135 @@ def get_credentials():
 
 def build_stock_spread_sheet():
 
-   all_listed = stock_list.parse_file()
+	all_listed = stock_list.parse_file()
 
-   """Shows basic usage of the Sheets API.
+	"""Shows basic usage of the Sheets API.
 
-   Creates a Sheets API service object and prints the names and majors of
-   students in a sample spreadsheet:
-   https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-   """
-   credentials = get_credentials()
-   http = credentials.authorize(httplib2.Http())
+	Creates a Sheets API service object and prints the names and majors of
+	students in a sample spreadsheet:
+	https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+	"""
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
 
-   discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                  'version=v4')
+	discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+						'version=v4')
 
-   service = discovery.build('sheets', 'v4', http=http,
-                           discoveryServiceUrl=discoveryUrl)
+	service = discovery.build('sheets', 'v4', http=http,
+									discoveryServiceUrl=discoveryUrl)
 
-   spreadsheet_id = '1gG6PIAKMKA0lBGRj-5X9UBM6DiELpbqktThJM_sWM6Q'
+	spreadsheet_id = '1gG6PIAKMKA0lBGRj-5X9UBM6DiELpbqktThJM_sWM6Q'
 
-   stock_values = all_listed.keys()
-   
-   major_dimension = "COLUMNS"
-   
-   data = [
-      "stocks",
-      "price",
-      "priceopen",
-      "high",
-      "low",
-      "volume",
-      "datedelay",
-      "change",
-      "changepct",
-      "closeyest",
-      "shares",
-   ]
+	stock_values = all_listed.keys()
 
-   values = []
-   
-   for data_name in data:
-      print("{} ======== ".format(data_name))
-      array_data = [ data_name ]
-      if data_name is "stocks":
-         for stock in stock_values:
-            array_data.append(stock)
-      else:
-         for stock in stock_values:
-            array_data.append("=GoogleFinance(\"{}\";\"{}\")".format(stock, data_name))
-   
-      values.append(array_data)
+	major_dimension = "COLUMNS"
 
-   init_index = 1
-   size = len(values[0])
+	# price: market price of the stock – delayed by up to 20 minutes.
+	# priceopen: the opening price of the stock for the current day.
+	# high: the highest price the stock traded for the current day.
+	# low: the lowest price the stock traded for the current day.
+	# volume: number of shares traded of this stock for the current day.
+	# marketcap: the market cap of the stock.
+	# tradetime: the last time the stock traded.
+	# datadelay: the delay in the data presented for this stock using the googleFinance() function.
+	# volumeavg: the average volume for this stock.
+	# pe: the Price-to-Earnings ratio for this stock.
+	# eps: the earnings-per-share for this stock.
+	# high52: the 52-week high for this stock.
+	# low52: the 52-week low for this stock.
+	# change: the change in the price of this stock since yesterday’s market close.
+	# beta: the beta value of this stock.
+	# changepct: the percentage change in the price of this stock since yesterday’s close.
+	# closeyest: yesterday’s closing price of this stock.
+	# shares: the number of shares outstanding of this stock.
+	# currency: the currency in which this stock is traded.
 
-   first_letter = ascii_uppercase[0]
-   last_letter = ascii_uppercase[len(data)-1]
+	data = [
+		"stocks",
+		"price",
+		"priceopen",
+		"high",
+		"low",
+		"volume",
+		"marketcap",
+		"tradetime",
+		"datadelay",
+		"volumeavg",
+		"pe",
+		"eps",
+		"high52",
+		"low52",
+		"change",
+		"beta",
+		"changepct",
+		"closeyest",
+		"shares",
+		"currency"
+	]
 
-   range_name = '{}{}:{}{}'.format(first_letter, init_index, last_letter, size + init_index)
-   value_input_option= "USER_ENTERED"
+	fold_size = 200
+	current_index = 0
+	init_index = 1
 
-   body = {
-      "range": range_name,
-      "majorDimension": major_dimension,
-      "values": values
-   }  
+	while current_index < len(stock_values):
+		
+		print("Populating Fold #{}".format(current_index % fold_size))
+		values = []
+		
+		for data_name in data:
+			print("Building Column {} ======== ".format(data_name))
+			array_data = [ data_name ]
+			if (data_name is "stocks") and (current_index is not 0):
+				row_index = 0
+				for stock in stock_values:
+					array_data.append(stock)
+					row_index = row_index + 1
+					if row_index is fold_size:
+						break
+			elif (data_name is not "stocks"):
+				row_index = 0
+				for stock in stock_values:
+					array_data.append("=GoogleFinance(\"{}\";\"{}\")".format(stock, data_name))
+					row_index = row_index + 1
+					if row_index is fold_size:
+						break
+			else:
+				continue
+		
+			values.append(array_data)
+		
+		
+		size = len(values[0])
+		current_index += size
 
-   # result = service.spreadsheets().values().batchUpdate(
-   #    spreadsheetId=spreadsheetId, body=batch_update_values_request_body).execute()
-   #  
-   
-   result = service.spreadsheets().values().update(
-    spreadsheetId=spreadsheet_id, range=range_name,
-    valueInputOption=value_input_option, body=body).execute()
-    
-   values = result.get('responses', [])
+		first_letter = ascii_uppercase[0]
+		last_letter = ascii_uppercase[len(data)-1]
 
-   # if not values:
-   #    print('No data found.')
-   # else:
-   #    print(values)
+		range_name = '{}{}:{}{}'.format(first_letter, init_index, last_letter, init_index + size)
+		value_input_option= "USER_ENTERED"
+
+		init_index += size
+
+		body = {
+			"range": range_name,
+			"majorDimension": major_dimension,
+			"values": values
+		}  
+
+		# result = service.spreadsheets().values().batchUpdate(
+		#    spreadsheetId=spreadsheetId, body=batch_update_values_request_body).execute()
+		#  
+		
+		result = service.spreadsheets().values().update(
+			spreadsheetId=spreadsheet_id, range=range_name,
+			valueInputOption=value_input_option, body=body).execute()
+			
+		values = result.get('responses', [])
+
+		# if not values:
+		#    print('No data found.')
+		# else:
+		#    print(values)
 
 
 if __name__ == '__main__':
